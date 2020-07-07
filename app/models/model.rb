a5f9ed5905a470e1ef6da1c8fe972d916b2d82e0
github username <github email address>
2020-07-06 22:57:29 -0400

    validations for model model

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,14 @@
 class Model < ApplicationRecord
   has_many :bookings
+
+  validates :name, presense: true
+  validates :pictures, presense: true
+  validates :height, presense: true
+  validates :bust, presense: true
+  validates :waist, presense: true
+  validates :hip, presense: true
+  validates :shoe, presense: true
+  validates :eyes, presense: true
+  validates :hair, presense: true
+  validates :birthdate, presense: true
 end
