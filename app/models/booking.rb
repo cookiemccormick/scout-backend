b4a481ef24e60a6608405ec3993a9e7350d01852
github username <github email address>
2020-07-06 22:54:36 -0400

    added validations to booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :model
+
+  validates :model_id, presense: true
+  validates :job, presense: true
+  validates :amount, presense: true
+  validates :date, presense: true
+  validates :time, presense: true
+  validates :description, presense: true
 end
