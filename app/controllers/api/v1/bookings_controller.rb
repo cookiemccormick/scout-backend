b87aed98d430666b0a30c72e2c04837f97c74204
github username <github email address>
2020-07-07 22:49:46 -0400

    Update bookings_controller.rb

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -1,2 +1,44 @@
 class Api::V1::BookingsController < ApplicationController
+  before_action :set_model
+
+  def create
+    @booking = @model.bookings.new(booking_params)
+    if @booking.save
+      render json: @booking
+    else
+      render json: {error: 'Error creating booking'}
+    end
+  end
+
+  def index
+    @bookings = @model.bookings
+    render json: @bookings
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+    render json: @booking
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(
+      :model_id,
+      :job,
+      :amount,
+      :start_time,
+      :end_time,
+      :description
+    )
+  end
+
+  def set_model
+    @model = Model.find(params[:model_id])
+  end
 end
