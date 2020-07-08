3acd95a6788e604362517b845ef3b95860096ef7
github username <github email address>
2020-07-07 22:21:18 -0400

    setup models controller

--- app/controllers/api/v1/models_controller.rb
+++ app/controllers/api/v1/models_controller.rb
@@ -1,2 +1,41 @@
 class Api::V1::ModelsController < ApplicationController
+  def create
+    @model = Model.new(model_params)
+    if @model.save
+      render json: @model
+    else
+      render json: {error: 'Error creating model'}
+    end
+  end
+
+  def index
+    @models = Models.all
+    render json: @models
+  end
+
+  def show
+    @model = Model.find(params[:id])
+    render json: @model
+  end
+
+  def destroy
+    @model = Model.find(params[:id])
+    @model.destroy
+  end
+
+  private
+
+  def model_params
+    params.require(:model).permit(
+      :name,
+      :pictures,
+      :height,
+      :bust,
+      :waist,
+      :hip,
+      :shoe,
+      :eyes,
+      :hair
+    )
+  end
 end
